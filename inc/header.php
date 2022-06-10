41b49c6132e72bd04ef857fdc984389c2743d35d
Useol <usus_us@naver.com>
2022-06-10 23:59:53 +0900

    fist

---
+++ inc/header.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html xmlns="//www.w3.org/1999/xhtml" xml:lang="ko" lang="ko">
+<title>merry christmas</title>
+<head>
+  <meta name="robots" content="noindex, nofollow">
+  <meta name="viewport" content="width=device-width, user-scalable=no">
+  <!-- 파비콘 -->
+  <link rel="apple-touch-icon" sizes="57x57" href="/img/pavicon/apple-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="/img/pavicon/apple-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="/img/pavicon/apple-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="/img/pavicon/apple-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="/img/pavicon/apple-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="/img/pavicon/apple-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="/img/pavicon/apple-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="/img/pavicon/apple-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="/img/pavicon/apple-icon-180x180.png">
+  <link rel="icon" type="image/png" sizes="192x192"  href="/img/pavicon/android-icon-192x192.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/img/pavicon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="/img/pavicon/favicon-96x96.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/img/pavicon/favicon-16x16.png">
+  <link rel="manifest" href="/img/pavicon/manifest.json">
+  <meta name="msapplication-TileColor" content="#ffffff">
+  <meta name="msapplication-TileImage" content="/img/pavicon/ms-icon-144x144.png">
+  <meta name="theme-color" content="#ffffff">
+  <!-- xeicon -->
+  <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/xeicon@2.3.3/xeicon.min.css">
+  <!-- js -->
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <!-- css custom -->
+  <link rel="stylesheet" href="/css/common.css">
+  <link rel="stylesheet" href="/css/font.css">
+
+  <!-- aos -->
+  <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+  <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+</head>
+<body>
+
+<header id="header">
+  <div class="inner">
+    <div class="left">
+
+    </div>
+    <div class="center">
+      <h1>
+        <img src="\snata\img\logo.jpg" alt="">
+        <img src="\snata\img\logo.jpg" alt="">
+        <img src="\snata\img\logo.jpg" alt="">
+      </h1>
+    </div>
+    <div class="right">
+
+    </div>
+  </div>
+</header>
---
+++ index.php
@@ -0,0 +1,7 @@
+<? include '../snata/inc/header.php' ?>
+<link rel="stylesheet" href="/snata/css/index.css">
+<div class="main_animation">
+  <div class="inner">
+
+  </div>
+</div>
